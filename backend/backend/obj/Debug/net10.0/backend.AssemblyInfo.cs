a2e1141a7b5d943c766f857eb92d6043c6db721f
Ayoub-El-Bouzidi <elbouzidiayoub.solicode@gmail.com>
2025-12-05 17:02:31 +0100

    save

--- backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b505c2c5ed4aa962a297ea0dc6169eb5efc551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2932805ba20d5bdc03d2db5d29ede80f49ab18")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
